95ad2c1a71aa5a007694c5951a7ab34a5a7bfb69
Amalie S <amaliesmidth@gmail.com>
2018-08-29 15:58:38 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -8,18 +8,3 @@ def my_each(array)
 end
 
 
-
-# def hello_t(array)
-#   if block_given?
-#     i = 0
-
-#     while i < array.length
-#       yield(array[i])
-#       i = i + 1
-#     end
-
-#     array
-#   else
-#     puts "Hey! No block was given!"
-#   end
-# end
